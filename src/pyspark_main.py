344aae9f68f6078f7ae141c8837803366a85d9fe
nayefroqaya <n.roqaya@gmail.com>
2026-01-27 00:43:44 +0100

    add parsing code

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -240,7 +240,7 @@ def main():
     pd.set_option("display.max_colwidth", None)
 
     # ---------------- Project configuration ----------------
-    DATASET = 'TH_5G'
+    DATASET = 'BGL'
     DATASETS_FOLDER = 'datasets'
     round_id = '1'
     mode = 'M'
