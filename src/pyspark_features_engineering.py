eda51731140ffb09a96d53e012608b7b616fb99b
nayefroqaya <n.roqaya@gmail.com>
2026-01-29 11:06:27 +0100

    update AD model

--- src/pyspark_features_engineering.py
+++ src/pyspark_features_engineering.py
@@ -655,7 +655,7 @@ class FeaturesEngineering:
             # -----------------------------
             # 3. Thresholds (from normal)
             # -----------------------------
-            threshold = train_pca.approxQuantile("anomaly_score", [0.95], 0.01)[0]
+            threshold = train_pca.approxQuantile("anomaly_score", [0.85], 0.01)[0]
 
             print(f"\n✅ PCA anomaly threshold (99% quantile of normal): {threshold:.6f}")
 
