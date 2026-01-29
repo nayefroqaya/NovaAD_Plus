bae40a1e79413188c5f68ad9e40c6b6d7b14b50a
nayefroqaya <n.roqaya@gmail.com>
2026-01-29 15:06:12 +0100

    update AD model

--- src/pyspark_features_engineering.py
+++ src/pyspark_features_engineering.py
@@ -735,7 +735,7 @@ class FeaturesEngineering:
 
             print('Classification_report full training data (PCA novelty)')
             print(classification_report(y_train_truth, y_train, digits=3))
-            exit()
+            #exit()
 
 
             '''
