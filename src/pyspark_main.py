fdec9bcb194ca490dacf7df36807e7b7193e6e0f
nayefroqaya <n.roqaya@gmail.com>
2026-01-30 21:25:18 +0100

    update AD model

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -241,9 +241,9 @@ def main():
     pd.set_option("display.max_colwidth", None)
 
     # ---------------- Project configuration ----------------
-    DATASET = 'TH_1G'
+    DATASET = 'HDFS'
     DATASETS_FOLDER = 'datasets'
-    round_id = '3'
+    round_id = '1'
     mode = 'x'
     Mix_or_stable = '0'
 
