e1a5aa5ccba6582440dffff437f3a00070c2f354
nayefroqaya <n.roqaya@gmail.com>
2026-01-30 10:56:34 +0100

    update AD model

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -241,9 +241,9 @@ def main():
     pd.set_option("display.max_colwidth", None)
 
     # ---------------- Project configuration ----------------
-    DATASET = 'BGL'
+    DATASET = 'SP_150MB'
     DATASETS_FOLDER = 'datasets'
-    round_id = '3'
+    round_id = '1'
     mode = 'x'
     Mix_or_stable = '0'
 
@@ -272,7 +272,7 @@ def main():
     #print(' Reading the file was done successfully ')
     #exit()
 
-    '''
+
     # ---------------- Load CSV into Spark ----------------
     all_data_df = spark.read.csv(ALL_DATASET_CSV_PATH, header=True, inferSchema=True).cache()
     all_data_df.count()  # Materialize cache
@@ -342,7 +342,7 @@ def main():
     extract_features_spill_gb = get_spill_size_gb(SPILL_DIR)
     print(f"Shuffle Spill during features extracting : {extract_features_spill_gb:.2f} GB")
     exit()
-    '''
+
 
 
     # ---------------- Load feature PKL → Spark ----------------
