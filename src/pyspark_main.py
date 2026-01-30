3aab811f5440ded14c73c965528251c564c025e8
Nayef Roqaya <n.roqaya@gmail.com>
2026-01-30 12:09:42 +0100

    AD update model

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -243,7 +243,7 @@ def main():
     # ---------------- Project configuration ----------------
     DATASET = 'SP_150MB'
     DATASETS_FOLDER = 'datasets'
-    round_id = '1'
+    round_id = '3'
     mode = 'x'
     Mix_or_stable = '0'
 
