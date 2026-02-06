09b207832f44039da5e4120d69dec241048406f4
nayefroqaya <n.roqaya@gmail.com>
2026-02-06 21:00:24 +0100

    update Novelty

--- src/pyspark_features_extracting.py
+++ src/pyspark_features_extracting.py
@@ -512,7 +512,7 @@ class FeaturesExtractor:
 
         print(f"[RESULT] Selected PCA components (best_k): {best_k}")
         '''
-        candidate_ks = [10, 20, 40, 50]
+        candidate_ks = [10, 20, 40, 50,60,70]
         target_variance = 0.999
         feature_col="vector_emb"
         best_k = None
