73562b6760bffd6f2ce04ed3e4a6102f60a6e7b0
nayefroqaya <n.roqaya@gmail.com>
2026-02-20 21:31:57 +0100

    update Novelty

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -241,7 +241,7 @@ def main():
     pd.set_option("display.max_colwidth", None)
 
     # ---------------- Project configuration ----------------
-    DATASET = 'SP_150MB'
+    DATASET = 'BGL'
     DATASETS_FOLDER = 'datasets'
     round_id = '1'
     mode = 'X'
