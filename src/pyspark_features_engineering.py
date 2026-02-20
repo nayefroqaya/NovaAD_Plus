ae9a06966391732218d8a3a19c664a2eb461abe9
nayefroqaya <n.roqaya@gmail.com>
2026-02-20 10:47:12 +0100

    update Novelty

--- src/pyspark_features_engineering.py
+++ src/pyspark_features_engineering.py
@@ -399,7 +399,8 @@ class FeaturesEngineering:
             # ============================================================
             # 1) Choose PCA k on NORMAL-fit only
             # ============================================================
-            candidate_ks = [ 50, 60, 70, 80,90]
+            candidate_ks = [10, 20, 40, 50, 60, 70]
+            #candidate_ks = [ 50, 60, 70, 80,90]
             target_variance = 0.999
 
             best_k = None
--- src/pyspark_features_extracting.py
+++ src/pyspark_features_extracting.py
@@ -512,8 +512,8 @@ class FeaturesExtractor:
 
         print(f"[RESULT] Selected PCA components (best_k): {best_k}")
         '''
-        #candidate_ks = [10, 20, 40, 50,60,70]
-        candidate_ks = [50, 60, 70, 80,90]
+        candidate_ks = [10, 20, 40, 50,60,70]
+        #candidate_ks = [40, 50, 60, 70]
 
         target_variance = 0.999
         feature_col="vector_emb"
