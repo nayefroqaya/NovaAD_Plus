3dbd4d4c3979c254c3b21c0826ef3f3c74a5c34c
nayefroqaya <n.roqaya@gmail.com>
2026-02-20 22:14:02 +0100

    update Novelty

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -241,7 +241,7 @@ def main():
     pd.set_option("display.max_colwidth", None)
 
     # ---------------- Project configuration ----------------
-    DATASET = 'HDFS'
+    DATASET = 'SP_100MB'
     DATASETS_FOLDER = 'datasets'
     round_id = '1'
     mode = 'X'
