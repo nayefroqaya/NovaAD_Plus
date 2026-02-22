72b8b27827bc95c99d4b88890aee095bb323ef19
nayefroqaya <n.roqaya@gmail.com>
2026-02-22 13:19:05 +0100

    update Novelty

--- src/pyspark_features_engineering.py
+++ src/pyspark_features_engineering.py
@@ -813,7 +813,8 @@ class FeaturesEngineering:
                 print(
                     "[WARN] sequences_df has no 'Label' column -> cannot compute final training classification report.")
 
-            return df_final_train_cls, df_test_cls, df_val_cls
+            #return df_final_train_cls, df_test_cls, df_val_cls
+            return df_train_quality, df_test_cls, df_val_cls
 
 
 
