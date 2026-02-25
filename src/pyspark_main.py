f56563d6e255c685326741d63f5ec1d6c6379f09
nayefroqaya <n.roqaya@gmail.com>
2026-02-25 23:26:56 +0100

    update Novelty

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -187,7 +187,7 @@ spark = (
     .config("spark.jars.repositories","https://mmlspark.azureedge.net/maven")# optional but often helps avoid dependency clashes
     .config("spark.jars.excludes", ",".join(
         ["org.scala-lang:scala-reflect", "org.apache.spark:spark-tags_2.12", "org.scalactic:scalactic_2.12",
-            "org.scalatest:scalatest_2.12", "com.fasterxml.jackson.core:jackson-databind"])
+            "org.scalatest:scalatest_2.12", "com.fasterxml.jackson.core:jackson-databind"]))
     .getOrCreate()
 )
 #exit()
