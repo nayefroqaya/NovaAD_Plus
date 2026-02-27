336dec60e4f9e7de485f673df43c628e6687de6b
nayefroqaya <n.roqaya@gmail.com>
2026-02-27 13:13:28 +0100

    update Novelty

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -279,7 +279,7 @@ def main():
     #print(' Reading the file was done successfully ')
     #exit()
 
-
+    '''
     # ---------------- Load CSV into Spark ----------------
     all_data_df = spark.read.csv(ALL_DATASET_CSV_PATH, header=True, inferSchema=True).cache()
     all_data_df.count()  # Materialize cache
@@ -352,6 +352,7 @@ def main():
     extract_features_spill_gb = get_spill_size_gb(SPILL_DIR)
     print(f"Shuffle Spill during features extracting : {extract_features_spill_gb:.2f} GB")
     exit()
+    '''
 
 
     # ---------------- Load feature PKL → Spark ----------------
