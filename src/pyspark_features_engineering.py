21a0f62cd9fcffcb97eff6fdd5901a0825952ba6
nayefroqaya <n.roqaya@gmail.com>
2026-02-27 11:56:02 +0100

    update Novelty

--- src/pyspark_features_engineering.py
+++ src/pyspark_features_engineering.py
@@ -822,6 +822,11 @@ class FeaturesEngineering:
                     lower(trim(col(GT_COL))).isin("normal", "0", "false", "no"), lit(0)).otherwise(
                     col(GT_COL).cast("int")))
 
+            sequences_df = sequences_df.withColumn("Label",
+                when(col("Label") == "normal", 0).when(col("Label") == "anomaly", 1).otherwise(None)
+                # or keep original if needed
+            )
+
             sequences_df.printSchema()
             train_counts = sequences_df.groupBy("Label").count()
             print("=== Label Distribution (All Data) ===")
