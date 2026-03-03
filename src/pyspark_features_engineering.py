cbb78999b61ceaa9e3b1516799bbc286c58f5b9c
nayefroqaya <n.roqaya@gmail.com>
2026-03-03 08:48:50 +0100

    update Novelty

--- src/pyspark_features_engineering.py
+++ src/pyspark_features_engineering.py
@@ -1017,7 +1017,7 @@ class FeaturesEngineering:
             print("\n=== Classification_report on FULL TRAIN ===")
             print(classification_report(pdf_full["true_label"], pdf_full["Final_Label"], digits=3))
 
-            #exit()
+            exit()
 
             # ------- classification stage. GBTClassifier-----New ------------------------------------------------------
             # ======================================
