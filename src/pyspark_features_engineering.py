cd382639f4f829d416927f7962a6408d6b0a995f
nayefroqaya <n.roqaya@gmail.com>
2026-03-03 13:10:01 +0100

    update Novelty

--- src/pyspark_features_engineering.py
+++ src/pyspark_features_engineering.py
@@ -543,11 +543,17 @@ class FeaturesEngineering:
             # ======================================
             # 9) Build full labeled train dataset
             # ======================================
-            df_normal_labeled_features = train_normal_df.select(col(id_col), col(feature_col).alias(
-                "features_vec_final")).withColumn("Final_Label", lit(0).cast("int"))
-            df_unlabeled_labeled_features = train_unlabeled_pca.select(col(id_col),
-                                                                       col(feature_col).alias("features_vec_final"),
-                                                                       col("Final_Label"))
+            #*df_normal_labeled_features = train_normal_df.select(col(id_col), col(feature_col).alias(
+            #*    "features_vec_final")).withColumn("Final_Label", lit(0).cast("int"))
+            df_normal_labeled_features = train_normal_df.withColumn("Final_Label", lit(0).cast("int"))
+
+            #*df_unlabeled_labeled_features = train_unlabeled_pca.select(col(id_col),
+            #*                                                           col(feature_col).alias("features_vec_final"),
+            #*                                                           col("Final_Label"))
+
+            df_unlabeled_labeled_features = train_unlabeled_pca.withColumn("Final_Label",
+                                                                           col("Final_Label").cast("int"))
+
             df_full_train_labeled_features = df_normal_labeled_features.unionByName(df_unlabeled_labeled_features)
 
             # ======================================
@@ -565,7 +571,7 @@ class FeaturesEngineering:
 
             df_full_train_labeled_features.printSchema()
 
-            exit()
+            #exit()
             # ------- classification stage. GBTClassifier-----New / updated --------------------------------------------
             # ======================================
             # 0) Prepare training set
