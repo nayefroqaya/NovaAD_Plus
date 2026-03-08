4e4920de71e33871053d2d752978ec9dc7b0234c
nayefroqaya <n.roqaya@gmail.com>
2026-03-08 17:16:34 +0100

    update Novelty

--- src/pyspark_features_engineering.py
+++ src/pyspark_features_engineering.py
@@ -1768,7 +1768,8 @@ class FeaturesEngineering:
             #    seed=123, subsamplingRate= 0.9,  #1.0
             #                             featureSubsetStrategy="all")
 
-            gbt = FMClassifier(featuresCol=features_col, labelCol="Final_Label", stepSize=0.01, factorSize=8,
+            gbt = FMClassifier(featuresCol=features_col, labelCol="Final_Label", stepSize=0.1, #0.01
+                               factorSize=8,
                 # dimension of factor vectors
                 maxIter=75)
 
