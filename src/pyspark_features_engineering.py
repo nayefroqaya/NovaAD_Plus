0de5a16f50b72d628386e1e9c19e0e7df6736c8f
nayefroqaya <n.roqaya@gmail.com>
2026-03-09 09:10:26 +0100

    update Novelty

--- src/pyspark_features_engineering.py
+++ src/pyspark_features_engineering.py
@@ -1652,7 +1652,7 @@ class FeaturesEngineering:
             gmm_v = val_pdf["gmm_flag"].values.astype(int) if "gmm_flag" in val_pdf.columns else np.zeros_like(
                 y_val)
 
-            TARGET_RECALL =  0.92 #94 0.95
+            TARGET_RECALL =  0.94 #94 0.95
             best_threshold, best_prec = 0.9, -1.0   # 0.5
 
             for t in np.arange(0.01, 0.999, 0.005):
