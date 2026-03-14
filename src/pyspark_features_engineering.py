05227fa056848d9a93b3b8ae805487ddbfab26e9
nayefroqaya <n.roqaya@gmail.com>
2026-03-14 14:35:36 +0100

    update Novelty

--- src/pyspark_features_engineering.py
+++ src/pyspark_features_engineering.py
@@ -1165,8 +1165,8 @@ class FeaturesEngineering:
             #                    featureSubsetStrategy="all")
 
             gbt = GBTClassifier(featuresCol=features_col, labelCol="Final_Label", weightCol="classWeight", maxIter=50,
-                maxDepth=6, stepSize=0.1, seed=42, subsamplingRate=1.0, featureSubsetStrategy="all", cacheNodeIds=True,
-                checkpointInterval=10)
+                maxDepth=6, stepSize=0.1, seed=42, subsamplingRate=1.0, featureSubsetStrategy="sqrt", cacheNodeIds=True,
+                checkpointInterval=10,  maxMemoryInMB=1024)
 
             #gbt = RandomForestClassifier(featuresCol=features_col, labelCol="Final_Label", weightCol="classWeight",
             #    numTrees=75, maxDepth=5, seed=SEED, subsamplingRate=0.9, featureSubsetStrategy="all")
