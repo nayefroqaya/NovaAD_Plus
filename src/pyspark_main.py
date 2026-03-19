beca5964ffc89aa17d8736468dfe64ce5b20da6f
nayefroqaya <n.roqaya@gmail.com>
2026-03-19 14:03:20 +0100

    update Novelty

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -366,7 +366,7 @@ def main():
     # ---------------- Project configuration ----------------
     DATASET = 'BGL'
     DATASETS_FOLDER = 'datasets'
-    round_id = '1'
+    round_id = '2'
     mode = 'X'
     Mix_or_stable = '0'
 
