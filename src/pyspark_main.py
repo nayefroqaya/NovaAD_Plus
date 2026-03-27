15edb07258e3025ff0944085db19f6b95cc11da8
nayefroqaya <n.roqaya@gmail.com>
2026-03-27 09:39:01 +0100

    update info

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -362,7 +362,7 @@ def main():
     # ---------------- Project configuration ----------------
     DATASET = 'SP_150MB'
     DATASETS_FOLDER = 'datasets'
-    round_id = '1'
+    round_id = '2'
     mode = 'X'
     Mix_or_stable = '0'
 
