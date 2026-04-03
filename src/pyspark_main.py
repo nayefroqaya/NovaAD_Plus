ee997ec2e4adffcdcbd6c4722d876aa2aa1ed8c0
nayefroqaya <n.roqaya@gmail.com>
2026-04-03 20:47:24 +0200

    update info

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -360,7 +360,7 @@ def main():
     pd.set_option("display.max_colwidth", None)
 
     # ---------------- Project configuration ----------------
-    DATASET = 'BGL'
+    DATASET = 'TH_1G'
     DATASETS_FOLDER = 'datasets'
     round_id = '1'
     mode = 'X'
