a1c9fcd7f0bb68963790c665c238d29b3c8a6175
nayefroqaya <n.roqaya@gmail.com>
2026-04-08 14:09:00 +0200

    reformat the code

--- src/pyspark_features_extracting.py
+++ src/pyspark_features_extracting.py
@@ -86,8 +86,8 @@ YELLOW = colorama.Fore.YELLOW
 
 class FeaturesExtractor:
 
-    @staticmethod
-    def features_extracting_configuring_tuning(round_id,features_extracting_obj,
+   # @staticmethod
+    def features_extracting_configuring_tuning(self, round_id,features_extracting_obj,
                                                Dataset_name, pre_final_global_features_pkl_path, df_features, spark):
 
         """
