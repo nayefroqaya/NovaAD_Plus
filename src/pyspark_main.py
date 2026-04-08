fdb128bbc5f96c1248ca93fb841a458d419c3080
nayefroqaya <n.roqaya@gmail.com>
2026-04-08 23:54:03 +0200

    reformat the code

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -357,9 +357,9 @@ def main():
     pd.set_option("display.max_colwidth", None)
 
     # ---------------- Project configuration ----------------
-    DATASET = 'HDFS'
+    DATASET = 'BGL'
     DATASETS_FOLDER = 'datasets'
-    round_id = '2'
+    round_id = '1'
     mode = 'X'
     Mix_or_stable = '0'
 
@@ -384,7 +384,7 @@ def main():
     model_evaluation_obj = ModelEvaluation()
     utilities_obj = Utilities()
 
-    '''
+
 
     # ---------------- Data as CSV ----------------
     # logdata_read_obj.read_original_data_log_from_log_to_csv(DATASET, ALL_DATASET_CSV_PATH)
@@ -477,7 +477,7 @@ def main():
     #print(f"Shuffle Spill during features extracting : {extract_features_spill_gb:.2f} GB")
     spark.stop()
     exit()
-    '''
+
 
     # ---------------- Load feature PKL → Spark ----------------
     # ✅ Load from Parquet
