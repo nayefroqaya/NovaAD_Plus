15d39763305d4088656f7e9046df88b8f72ff83d
nayefroqaya <n.roqaya@gmail.com>
2026-04-09 21:06:51 +0200

    reformat the code

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -359,7 +359,7 @@ def main():
     # ---------------- Project configuration ----------------
     DATASET = 'SP_150MB'
     DATASETS_FOLDER = 'datasets'
-    round_id = '1'
+    round_id = '2'
     mode = 'X'
     Mix_or_stable = '0'
 
