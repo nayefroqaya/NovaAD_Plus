cd36ba5a87766f0589f8379095acc03984cb7ac8
nayefroqaya <n.roqaya@gmail.com>
2026-04-09 23:45:09 +0200

    reformat the code

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -42,9 +42,9 @@ print("Detected RAM:", total_ram_gb, "GB")
 # ============================================================
 
 
-num_workers = 4     # defualt =8
+num_workers = 8    # defualt =8
 cores_per_worker = 4   # defaul =4  # cores_per_worker = spark_cores // num_workers
-executor_memory_gb = 20  # executor_memory_gb = spark_ram_gb // num_workers
+executor_memory_gb = 10  # executor_memory_gb = spark_ram_gb // num_workers
 driver_memory_gb = 20  # driver_memory_gb = executor_memory_gb
 worker_memory_mib = executor_memory_gb * 1024  # worker_memory_mib = executor_memory_gb * 1024
 
