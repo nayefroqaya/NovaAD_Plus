0fb5461330f55f704812afe89c50525e6f54f884
nayefroqaya <n.roqaya@gmail.com>
2026-04-09 20:17:18 +0200

    reformat the code

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -357,9 +357,9 @@ def main():
     pd.set_option("display.max_colwidth", None)
 
     # ---------------- Project configuration ----------------
-    DATASET = 'SP_150MB'
+    DATASET = 'SP_100MB'
     DATASETS_FOLDER = 'datasets'
-    round_id = '3'
+    round_id = '1'
     mode = 'X'
     Mix_or_stable = '0'
 
@@ -385,7 +385,7 @@ def main():
     utilities_obj = Utilities()
 
 
-
+    '''
     # ---------------- Data as CSV ----------------
     # logdata_read_obj.read_original_data_log_from_log_to_csv(DATASET, ALL_DATASET_CSV_PATH)
     # print(' Reading the file was done successfully ')
@@ -477,6 +477,7 @@ def main():
     #print(f"Shuffle Spill during features extracting : {extract_features_spill_gb:.2f} GB")
     spark.stop()
     exit()
+    '''
 
 
     # ---------------- Load feature PKL → Spark ----------------
