eaab41488a716334326df1c15e49c363458f0829
nayefroqaya <n.roqaya@gmail.com>
2026-04-11 01:01:43 +0200

    reformat the code

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -42,9 +42,9 @@ print("Detected RAM:", total_ram_gb, "GB")
 # ============================================================
 
 
-num_workers = 1   # defualt =8
-cores_per_worker =1   # defaul =4  # cores_per_worker = spark_cores // num_workers
-executor_memory_gb =1 # executor_memory_gb = spark_ram_gb // num_workers
+num_workers = 2  # defualt =8
+cores_per_worker =2   # defaul =4  # cores_per_worker = spark_cores // num_workers
+executor_memory_gb =10 # executor_memory_gb = spark_ram_gb // num_workers
 driver_memory_gb = 20  # driver_memory_gb = executor_memory_gb
 worker_memory_mib = executor_memory_gb * 1024  # worker_memory_mib = executor_memory_gb * 1024
 
@@ -357,7 +357,7 @@ def main():
     pd.set_option("display.max_colwidth", None)
 
     # ---------------- Project configuration ----------------
-    DATASET = 'SP_100MB'
+    DATASET = 'SP_150MB'
     DATASETS_FOLDER = 'datasets'
     round_id = '1'
     mode = 'X'
