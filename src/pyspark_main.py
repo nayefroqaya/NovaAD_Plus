1aeeaf588018744a6d6e21d6da35dde56ac71088
nayefroqaya <n.roqaya@gmail.com>
2026-04-14 09:47:52 +0200

    update in the  dataset code

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -384,7 +384,7 @@ def main():
     model_evaluation_obj = ModelEvaluation()
     utilities_obj = Utilities()
 
-    '''
+
     # ---------------- Data as CSV ----------------
     #logdata_read_obj.read_original_data_log_from_log_to_csv(DATASET, ALL_DATASET_CSV_PATH)
     #print(' Reading the file was done successfully ')
@@ -478,7 +478,7 @@ def main():
     #print(f"Shuffle Spill during features extracting : {extract_features_spill_gb:.2f} GB")
     spark.stop()
     exit()
-    '''
+
 
 
 
