2183339de399efa285787cdae15ce13060165a1c
nayefroqaya <n.roqaya@gmail.com>
2026-04-14 14:39:18 +0200

    update in the  dataset code

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -147,7 +147,7 @@ def main():
     # ---------------- Project configuration ----------------
     DATASET = 'TH_1G_ratio'
     DATASETS_FOLDER = 'datasets'
-    round_id = '1'
+    round_id = '2'
     mode = 'X'
     Mix_or_stable = '0'
 
