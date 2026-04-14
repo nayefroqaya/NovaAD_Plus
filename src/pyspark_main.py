5d808fc6c256850d2704ee3398af562c5cf2ed1d
nayefroqaya <n.roqaya@gmail.com>
2026-04-15 00:24:44 +0200

    update in the  dataset code

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -176,8 +176,8 @@ def main():
     # ---------------- Data as CSV ----------------
     logdata_read_obj.read_original_data_log_from_log_to_csv(DATASET, ALL_DATASET_CSV_PATH)
     print(' Reading the file was done successfully ')
-    spark.stop()
-    exit()
+    #spark.stop()
+    #exit()
  
     # ---------------- Load CSV into Spark ----------------
     all_data_df = spark.read.csv(ALL_DATASET_CSV_PATH, header=True, inferSchema=True).cache()
