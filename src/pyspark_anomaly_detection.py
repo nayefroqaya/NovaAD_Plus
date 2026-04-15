99f0b604c1943d062b75d16d6642da572c2055bd
nayefroqaya <n.roqaya@gmail.com>
2026-04-15 17:10:45 +0200

    update in the  dataset code

--- src/pyspark_anomaly_detection.py
+++ src/pyspark_anomaly_detection.py
@@ -161,13 +161,13 @@ class AnomalyDetector:
         # --------------------------
         # 0.2) Weighting (cap weights to reduce swings)
         # --------------------------
-        PSEUDO_TRUST = 0.3 #0.6
-        WEIGHT_CAP =  4.0 #8.0  # smaller cap = more stable, fewer crazy shifts
+        PSEUDO_TRUST = 0.6
+        WEIGHT_CAP = 8.0  # smaller cap = more stable, fewer crazy shifts
 
         n0 = train_df.filter(col("Final_Label") == 0).count()
         n1 = train_df.filter(col("Final_Label") == 1).count()
 
-        raw_w1 = float(n0 / max(n1, 1)) *  0.35 #0.7
+        raw_w1 = float(n0 / max(n1, 1)) * 0.7
         w1 = float(min(raw_w1, WEIGHT_CAP))
         w0 = 1.0
 
@@ -250,9 +250,7 @@ class AnomalyDetector:
 
         gbt = SparkXGBClassifier(features_col=features_col, label_col="Final_Label", weight_col="classWeight",
                                  max_depth=4, eta=0.05, n_estimators=500, subsample=0.85, colsample_bytree=0.80,
-                                 scale_pos_weight= 1.0, #1.5,
-
-                                 eval_metric="logloss", seed=42)
+                                 scale_pos_weight=1.5, eval_metric="logloss", seed=42)
 
         # gbt = RandomForestClassifier(featuresCol=features_col, labelCol="Final_Label", weightCol="classWeight",
         #    numTrees=75, maxDepth=5, seed=SEED, subsamplingRate=0.9, featureSubsetStrategy="all")
