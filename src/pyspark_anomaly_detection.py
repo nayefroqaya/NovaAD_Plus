bf77aa6ed8b5de4b9991a9f1a9b5fbd3ed299c7f
nayefroqaya <n.roqaya@gmail.com>
2026-04-15 15:16:02 +0200

    update in the  dataset code

--- src/pyspark_anomaly_detection.py
+++ src/pyspark_anomaly_detection.py
@@ -243,17 +243,17 @@ class AnomalyDetector:
         #                    featureSubsetStrategy="all")
 
         from xgboost.spark import SparkXGBClassifier
-
+        from pyspark.ml.classification import RandomForestClassifier
         # gbt = SparkXGBClassifier(features_col=features_col, label_col="Final_Label", weight_col="classWeight",
         #    #num_workers=4,  # tune to your cluster
         #    max_depth=6, eta=0.1, n_estimators=200, subsample=1.0, colsample_bytree=1.0, seed=42)
 
-        gbt = SparkXGBClassifier(features_col=features_col, label_col="Final_Label", weight_col="classWeight",
-                                 max_depth=4, eta=0.05, n_estimators=500, subsample=0.85, colsample_bytree=0.80,
-                                 scale_pos_weight=1.5, eval_metric="logloss", seed=42)
+        #gbt = SparkXGBClassifier(features_col=features_col, label_col="Final_Label", weight_col="classWeight",
+        #                         max_depth=4, eta=0.05, n_estimators=500, subsample=0.85, colsample_bytree=0.80,
+        #                         scale_pos_weight=1.5, eval_metric="logloss", seed=42)
 
-        # gbt = RandomForestClassifier(featuresCol=features_col, labelCol="Final_Label", weightCol="classWeight",
-        #    numTrees=75, maxDepth=5, seed=SEED, subsamplingRate=0.9, featureSubsetStrategy="all")
+        gbt = RandomForestClassifier(featuresCol=features_col, labelCol="Final_Label", weightCol="classWeight",
+            numTrees=75, maxDepth=5, seed=42, subsamplingRate=0.9, featureSubsetStrategy="all")
 
         # gbt = FMClassifier(
         #            featuresCol=features_col,
@@ -263,21 +263,7 @@ class AnomalyDetector:
         #            maxIter=75
         #        )
 
-        # gbt = FMClassifier(
-        #     featuresCol=features_col,
-        #     labelCol="Final_Label",
-        #     factorSize=32,
-        #     regParam=0.001,
-        #    stepSize=0.03,
-        #    maxIter=150,
-        #    miniBatchFraction=0.5,
-        #    fitLinear=True,
-        #    fitIntercept=True,
-        #    solver="adamW",
-        #    probabilityCol="probability",
-        #    rawPredictionCol="rawPrediction",
-        #    predictionCol="prediction"
-        # )
+
 
         t0 = time.time()
         model = gbt.fit(train_base_df)
