6c3e56feaf76f34e01a64bd1cd4998fb2aae829d
nayefroqaya <n.roqaya@gmail.com>
2026-04-16 23:29:17 +0200

    code for test project

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -145,7 +145,7 @@ def main():
     pd.set_option("display.max_colwidth", None)
 
     # ---------------- Project configuration ----------------
-    DATASET = 'TH_5G_ratio'
+    DATASET = 'TH_6G_ratio'
     DATASETS_FOLDER = 'datasets'
     round_id = '1'
     mode = 'X'
@@ -174,8 +174,8 @@ def main():
 
 
     # ---------------- Data as CSV ----------------
-    #logdata_read_obj.read_original_data_log_from_log_to_csv(DATASET, ALL_DATASET_CSV_PATH)
-    #print(' Reading the file was done successfully ')
+    logdata_read_obj.read_original_data_log_from_log_to_csv(DATASET, ALL_DATASET_CSV_PATH)
+    print(' Reading the file was done successfully ')
     #spark.stop()
     #exit()
  
