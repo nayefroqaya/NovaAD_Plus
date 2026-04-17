811c396caf4a9a55c764e8318022b6774b228b04
nayefroqaya <n.roqaya@gmail.com>
2026-04-17 14:11:14 +0200

    code for test project

--- src/pyspark_anomaly_detection.py
+++ src/pyspark_anomaly_detection.py
@@ -285,7 +285,8 @@ class AnomalyDetector:
         pca_v = val_pdf["pca_flag"].values.astype(int) if "pca_flag" in val_pdf.columns else np.zeros_like(y_val)
         gmm_v = val_pdf["gmm_flag"].values.astype(int) if "gmm_flag" in val_pdf.columns else np.zeros_like(y_val)
 
-        TARGET_RECALL = 0.95  # 94 0.95
+        #***TARGET_RECALL = 0.95  # 94 0.95
+        TARGET_RECALL = 0.98
         best_threshold, best_prec = 0.5, -1.0  # 0.5
 
         for t in np.arange(0.01, 0.999, 0.005):
