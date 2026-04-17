e0596e73bc35683107c7e4543986f185b7a8380e
nayefroqaya <n.roqaya@gmail.com>
2026-04-17 09:00:39 +0200

    code for test project

--- src/load_datalog.py
+++ src/load_datalog.py
@@ -581,7 +581,7 @@ class LogdataRead:
             df.to_csv(All_dataset_path_as_csv, escapechar='\\', index=False)
 
 
-        elif dataset == 'TH_1G_ratio' or dataset == 'TH_2G_ratio' or dataset == 'TH_3G_ratio' or dataset == 'TH_6G_ratio' or dataset == 'TH_5G_ratio' or dataset == 'TH_10G_ratio':
+        elif dataset == 'TH_1G_ratio' or dataset == 'TH_2G_ratio' or dataset == 'TH_3G_ratio' or dataset == 'TH_5G_ratio' or dataset == 'TH_6G_ratio' or dataset == 'TH_10G_ratio':
             #  Define dtype mapping for efficient memory usage
             dtype_mapping = {"User": "str", "EventTemplate": "category", "Content": "str", "Date": "str", "Time": "str",
                              "Component": "category", "EventId": "str", "Label": "category"}
