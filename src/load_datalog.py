47786871592d9cc3e12235d0ae5fa7074a544b16
nayefroqaya <n.roqaya@gmail.com>
2026-04-18 22:39:33 +0200

    code for test project

--- src/load_datalog.py
+++ src/load_datalog.py
@@ -643,6 +643,7 @@ class LogdataRead:
             df=df_final  # new dataset with the new portion
             print(len(df))
             '''
+            '''
 
             r = 0.043
 
@@ -690,7 +691,7 @@ class LogdataRead:
 
             df = df_final
             print(f"Updated df length: {len(df)}")
-
+            '''
 
 
             n_total = len(df)
