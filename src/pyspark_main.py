b3d5e32e01809ec7127725bfb5dfbc2ce991941d
nayefroqaya <n.roqaya@gmail.com>
2026-04-21 22:38:12 +0200

    code for test project

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -145,7 +145,7 @@ def main():
     pd.set_option("display.max_colwidth", None)
 
     # ---------------- Project configuration ----------------
-    DATASET = 'TH_20G_ratio'
+    DATASET = 'TH_6G_ratio'
     DATASETS_FOLDER = 'datasets'
     round_id = '1'
     mode = 'X'
@@ -171,13 +171,13 @@ def main():
     anomaly_detection_obj = AnomalyDetector()
     model_evaluation_obj = ModelEvaluation()
     utilities_obj = Utilities()
-
+    '''
 
     # ---------------- Data as CSV ----------------
     logdata_read_obj.read_original_data_log_from_log_to_csv(DATASET, ALL_DATASET_CSV_PATH)
     print(' Reading the file was done successfully ')
-    spark.stop()
-    exit()
+    #spark.stop()
+    #exit()
 
     # ---------------- Load CSV into Spark ----------------
     all_data_df = spark.read.csv(ALL_DATASET_CSV_PATH, header=True, inferSchema=True).cache()
@@ -267,7 +267,7 @@ def main():
     #print(f"Shuffle Spill during features extracting : {extract_features_spill_gb:.2f} GB")
     #spark.stop()
     #exit()
-
+    '''
 
 
     
