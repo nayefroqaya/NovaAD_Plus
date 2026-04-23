4e72064800871d091be9becc672e58fe01ad1d24
nayefroqaya <n.roqaya@gmail.com>
2026-04-23 12:31:28 +0200

    code for test project

--- src/pyspark_anomaly_detection.py
+++ src/pyspark_anomaly_detection.py
@@ -341,9 +341,9 @@ class AnomalyDetector:
         for off in offset_grid:
             gate_t = min(best_threshold + float(off), 0.999)
 
-            gated_preds = ((p_val >= best_threshold) | ((p_val >= gate_t) & ((pca_v + gmm_v) >= 1))).astype(int)
+            #**gated_preds = ((p_val >= best_threshold) | ((p_val >= gate_t) & ((pca_v + gmm_v) >= 1))).astype(int)
 
-            #xxgated_preds = ((p_val >= gate_t) | ((p_val >= best_threshold) & ((pca_v + gmm_v) >= 1))).astype(int)
+            gated_preds = ((p_val >= gate_t) | ((p_val >= best_threshold) & ((pca_v + gmm_v) >= 1))).astype(int)
 
             f1g = f1_score(y_val, gated_preds, pos_label=1, zero_division=0)
             if f1g > best_f1_gate:
