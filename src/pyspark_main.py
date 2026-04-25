8529c13d04e2b1f05565f0de1216ba444124cd07
nayefroqaya <n.roqaya@gmail.com>
2026-04-25 21:15:33 +0200

    code for test project

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -147,7 +147,7 @@ def main():
     # ---------------- Project configuration ----------------
     DATASET = 'TH_1G'
     DATASETS_FOLDER = 'datasets'
-    round_id = '1'
+    round_id = '3'
     mode = 'X'
     Mix_or_stable = '0'
 
