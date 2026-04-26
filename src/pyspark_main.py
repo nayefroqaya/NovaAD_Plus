b7eb7f3b478943ac6ac59fb3c3d93c42c3dadc99
nayefroqaya <n.roqaya@gmail.com>
2026-04-26 03:26:29 +0200

    code for test project

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -145,9 +145,9 @@ def main():
     pd.set_option("display.max_colwidth", None)
 
     # ---------------- Project configuration ----------------
-    DATASET = 'SP_150MB_ratio'
+    DATASET = 'TH_3G_ratio'
     DATASETS_FOLDER = 'datasets'
-    round_id = '3'
+    round_id = '1'
     mode = 'X'
     Mix_or_stable = '0'
 
