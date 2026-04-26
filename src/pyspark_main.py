be66cd31a3481827c212ada3bdc937a071897780
nayefroqaya <n.roqaya@gmail.com>
2026-04-26 19:56:35 +0200

    code for test project

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -145,7 +145,7 @@ def main():
     pd.set_option("display.max_colwidth", None)
 
     # ---------------- Project configuration ----------------
-    DATASET = 'TH_20G_ratio'
+    DATASET = 'BGL'
     DATASETS_FOLDER = 'datasets'
     round_id = '1'
     mode = 'X'
