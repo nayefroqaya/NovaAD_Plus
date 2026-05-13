f3f61779c7804469e3ce618e22b56f83f9cb6d33
nayefroqaya <n.roqaya@gmail.com>
2026-05-13 15:05:26 +0200

    Update files

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -140,9 +140,9 @@ def main():
     pd.set_option("display.max_colwidth", None)
 
     # ---------------- Project configuration ----------------
-    DATASET = 'HDFS'
+    DATASET = 'BGL'
     DATASETS_FOLDER = 'datasets'
-    round_id = '3'
+    round_id = '1'
     mode = 'X'
     Mix_or_stable = '0'
 
