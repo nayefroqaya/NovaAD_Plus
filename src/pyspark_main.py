9d809a9b95e51177098c471bffc50591e3e503c4
nayefroqaya <n.roqaya@gmail.com>
2026-05-13 17:52:51 +0200

    Update files

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -142,7 +142,7 @@ def main():
     # ---------------- Project configuration ----------------
     DATASET = 'TH_3G_ratio'
     DATASETS_FOLDER = 'datasets'
-    round_id = '1'
+    round_id = '2'
     mode = 'X'
     Mix_or_stable = '0'
 
