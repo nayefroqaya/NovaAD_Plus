22cba475cc2ae83faba4d27f53e241b494b5c394
nayefroqaya <n.roqaya@gmail.com>
2026-06-19 17:16:58 +0200

    update

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -257,9 +257,9 @@ def main():
     #output_path = "gs://sparkadls/Nova_Plus/datasets/TH_12G_ratio/1_TH_12G_ratio_Topic_sentiment_diff_semantic_df.parquet"  # for cloud
 
     final_train_with_test_with_val = spark.read.parquet(output_path)
-    final_train_with_test_with_val.count()
+    #final_train_with_test_with_val.count()
     final_train_with_test_with_val.printSchema()
-    final_train_with_test_with_val.select("Label").distinct().show()
+    #final_train_with_test_with_val.select("Label").distinct().show()
     # final_train_with_test_with_val.select("year").distinct().show()
     # spark.stop()
     # exit()
@@ -268,7 +268,7 @@ def main():
     def repeat_df(df, n):
         return reduce(lambda a, b: a.unionByName(b), [df] * n)
 
-    final_train_with_test_with_val = repeat_df(final_train_with_test_with_val, 30)
+    final_train_with_test_with_val = repeat_df(final_train_with_test_with_val, 25)
     # ---------------- Features Engineering ----------------
 
     start_agree_trans = time.time()
