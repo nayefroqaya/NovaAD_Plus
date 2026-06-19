ff594934408f8fd77cf0c8e23ee8f3cea6a6da49
nayefroqaya <n.roqaya@gmail.com>
2026-06-19 16:34:49 +0200

    update

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -268,7 +268,7 @@ def main():
     def repeat_df(df, n):
         return reduce(lambda a, b: a.unionByName(b), [df] * n)
 
-    final_train_with_test_with_val = repeat_df(final_train_with_test_with_val, 50)
+    final_train_with_test_with_val = repeat_df(final_train_with_test_with_val, 100)
     # ---------------- Features Engineering ----------------
 
     start_agree_trans = time.time()
