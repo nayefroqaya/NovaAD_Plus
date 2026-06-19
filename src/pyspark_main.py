9f687b426e684ec3afcbef75d10eb28b7e661a50
nayefroqaya <n.roqaya@gmail.com>
2026-06-19 15:56:05 +0200

    update

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -142,7 +142,7 @@ def main():
     pd.set_option("display.max_colwidth", None)
 
     # ---------------- Project configuration ----------------
-    DATASET = 'TH_12G_ratio'
+    DATASET = 'SP_150MB_ratio'
     DATASETS_FOLDER = 'datasets'
     round_id = '1'
     mode = 'X'
@@ -251,10 +251,10 @@ def main():
     # ---------------- Load feature PKL → Spark ----------------
     # ✅ Load from Parquet
     #output_path = f'../{DATASETS_FOLDER}/{DATASET}/{round_id}_{DATASET}_Topic_sentiment_diff_semantic_df.parquet'  # round_id + '_' + DATASET + "_Topic_sentiment_diff_semantic_df.parquet"
-    #output_path = "gs://sparkadls/Nova_Plus/datasets/SP_150MB_ratio/1_SP_150MB_ratio_Topic_sentiment_diff_semantic_df.parquet"  # for cloud
+    output_path = "gs://sparkadls/Nova_Plus/datasets/SP_150MB_ratio/1_SP_150MB_ratio_Topic_sentiment_diff_semantic_df.parquet"  # for cloud
     #output_path = "gs://sparkadls/Nova_Plus/datasets/TH_1G/1_TH_1G_Topic_sentiment_diff_semantic_df.parquet"  # for cloud
     #output_path = "gs://sparkadls/Nova_Plus/datasets/TH_3G_ratio/1_TH_3G_ratio_Topic_sentiment_diff_semantic_df.parquet"  # for cloud
-    output_path = "gs://sparkadls/Nova_Plus/datasets/TH_12G_ratio/1_TH_12G_ratio_Topic_sentiment_diff_semantic_df.parquet"  # for cloud
+    #output_path = "gs://sparkadls/Nova_Plus/datasets/TH_12G_ratio/1_TH_12G_ratio_Topic_sentiment_diff_semantic_df.parquet"  # for cloud
 
     final_train_with_test_with_val = spark.read.parquet(output_path)
     final_train_with_test_with_val.count()
@@ -265,10 +265,10 @@ def main():
     # exit()
 
     # duplicating the dataset for scalability experiments :
-    #def repeat_df(df, n):
-    #    return reduce(lambda a, b: a.unionByName(b), [df] * n)
+    def repeat_df(df, n):
+        return reduce(lambda a, b: a.unionByName(b), [df] * n)
 
-    #final_train_with_test_with_val = repeat_df(final_train_with_test_with_val, 10)
+    final_train_with_test_with_val = repeat_df(final_train_with_test_with_val, 20)
     # ---------------- Features Engineering ----------------
 
     start_agree_trans = time.time()
@@ -301,11 +301,11 @@ def main():
     #sequences_df_path = f'../{DATASETS_FOLDER}/{DATASET}/{round_id}_{DATASET}_sequences_df.parquet'
 
     # Cloud server path
-    #df_full_train_labeled_features_path = "gs://sparkadls/Nova_Plus/datasets/SP_150MB_ratio/1_SP_150MB_ratio_df_full_train_labeled_features.parquet"
-    #sequences_df_path = "gs://sparkadls/Nova_Plus/datasets/SP_150MB_ratio/1_SP_150MB_ratio_sequences_df.parquet"
+    df_full_train_labeled_features_path = "gs://sparkadls/Nova_Plus/datasets/SP_150MB_ratio/1_SP_150MB_ratio_df_full_train_labeled_features.parquet"
+    sequences_df_path = "gs://sparkadls/Nova_Plus/datasets/SP_150MB_ratio/1_SP_150MB_ratio_sequences_df.parquet"
 
-    df_full_train_labeled_features_path = "gs://sparkadls/Nova_Plus/datasets/TH_9G_ratio/1_TH_9G_ratio_df_full_train_labeled_features.parquet"
-    sequences_df_path = "gs://sparkadls/Nova_Plus/datasets/TH_12G_ratio/1_TH_12G_ratio_sequences_df.parquet"
+    #df_full_train_labeled_features_path = "gs://sparkadls/Nova_Plus/datasets/TH_9G_ratio/1_TH_9G_ratio_df_full_train_labeled_features.parquet"
+    #sequences_df_path = "gs://sparkadls/Nova_Plus/datasets/TH_12G_ratio/1_TH_12G_ratio_sequences_df.parquet"
 
 
     # save df to path:
