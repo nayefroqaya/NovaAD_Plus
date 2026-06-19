b735b4ab7487a1eba233025ae81d5193737a0e52
nayefroqaya <n.roqaya@gmail.com>
2026-06-19 23:58:56 +0200

    update

--- src/pyspark_main.py
+++ src/pyspark_main.py
@@ -143,7 +143,7 @@ def main():
     pd.set_option("display.max_colwidth", None)
 
     # ---------------- Project configuration ----------------
-    DATASET = 'TH_3G_ratio'
+    DATASET = 'TH_9G_ratio'
     DATASETS_FOLDER = 'datasets'
     round_id = '1'
     mode = 'X'
@@ -254,7 +254,7 @@ def main():
     #output_path = f'../{DATASETS_FOLDER}/{DATASET}/{round_id}_{DATASET}_Topic_sentiment_diff_semantic_df.parquet'  # round_id + '_' + DATASET + "_Topic_sentiment_diff_semantic_df.parquet"
     #output_path = "gs://sparkadls/Nova_Plus/datasets/SP_150MB_ratio/1_SP_150MB_ratio_Topic_sentiment_diff_semantic_df.parquet"  # for cloud
    # output_path = "gs://sparkadls/Nova_Plus/datasets/TH_1G/1_TH_1G_Topic_sentiment_diff_semantic_df.parquet"  # for cloud
-    output_path = "gs://sparkadls/Nova_Plus/datasets/TH_3G_ratio/1_TH_3G_ratio_Topic_sentiment_diff_semantic_df.parquet"  # for cloud
+    output_path = "gs://sparkadls/Nova_Plus/datasets/TH_9G_ratio/1_TH_9G_ratio_Topic_sentiment_diff_semantic_df.parquet"  # for cloud
     #output_path = "gs://sparkadls/Nova_Plus/datasets/TH_12G_ratio/1_TH_12G_ratio_Topic_sentiment_diff_semantic_df.parquet"  # for cloud
 
     final_train_with_test_with_val = spark.read.parquet(output_path)
@@ -318,8 +318,8 @@ def main():
     #df_full_train_labeled_features_path = "gs://sparkadls/Nova_Plus/datasets/SP_150MB_ratio/1_SP_150MB_ratio_df_full_train_labeled_features.parquet"
     #sequences_df_path = "gs://sparkadls/Nova_Plus/datasets/SP_150MB_ratio/1_SP_150MB_ratio_sequences_df.parquet"
 
-    df_full_train_labeled_features_path = "gs://sparkadls/Nova_Plus/datasets/TH_3G_ratio/1_TH_3G_ratio_df_full_train_labeled_features.parquet"
-    sequences_df_path = "gs://sparkadls/Nova_Plus/datasets/TH_3G_ratio/1_TH_3G_ratio_sequences_df.parquet"
+    df_full_train_labeled_features_path = "gs://sparkadls/Nova_Plus/datasets/TH_9G_ratio/1_TH_9G_ratio_df_full_train_labeled_features.parquet"
+    sequences_df_path = "gs://sparkadls/Nova_Plus/datasets/TH_9G_ratio/1_TH_9G_ratio_sequences_df.parquet"
 
 
     # save df to path:
